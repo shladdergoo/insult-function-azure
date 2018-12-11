902f5e78fad0d6f24715df131c7e34693d759d39
James Fox <james.fox@datagility.co.uk>
2018-12-11 08:38:19 +0000

    adding logging

--- src/insultHttpTrigger/index.ts
+++ src/insultHttpTrigger/index.ts
@@ -6,6 +6,14 @@ import IInsultService from './interface/iinsultservice';
 import Types from './types';
 
 export const index = (context: any, req: any) => {
+  context.log(
+    `insultHttpTrigger function processed a request. RequestUri=${
+      req.originalUrl
+    }`
+  );
+  context.log(`Request Headers = ${JSON.stringify(req.headers)}`);
+  context.log(`Request Body = ${JSON.stringify(req.body)}`);
+
   const insultService = container.get<IInsultService>(Types.IInsultService);
   const insults = insultService.GetInsults();
 
